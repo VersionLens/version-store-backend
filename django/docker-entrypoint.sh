476ddaad9b562d75a42167e835947906dca82125
Fredrik Stockman <fredrikstockman@gmail.com>
2023-02-17 23:04:16 +0100

    Don't run seedstates in docker-entrypoint

--- django/docker-entrypoint.sh
+++ django/docker-entrypoint.sh
@@ -3,5 +3,5 @@
 python manage.py migrate --no-input
 DJANGO_SUPERUSER_USERNAME=demo DJANGO_SUPERUSER_EMAIL=foo@bar.com DJANGO_SUPERUSER_PASSWORD=demo python manage.py createsuperuser --no-input
 mkdir -p /app/media/products
-python manage.py seedstates
+# python manage.py seedstates
 python manage.py runserver 0.0.0.0:8000
